35300f0b0edd9b5d7d04f1b933a159d327e1c85d
Juhani Pelli <juhani.pelli@gmail.com>
2023-06-28 20:09:48 +0300

    Cleanup boring patterns

---
+++ song/edit.ts
@@ -0,0 +1,52 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+export async function editVerse(
+  input: string,
+  instruction: string,
+  temperature: number
+) {
+  let i = 0
+  while (i < 20) {
+    try {
+      const response = await openai.createEdit({
+        model: 'text-davinci-edit-001',
+        input,
+        instruction,
+        temperature,
+        top_p: 1,
+      })
+      if (response.status === 200 && response.data.choices.length > 0) {
+        const verse = response.data.choices[0].text.split('\n').slice(0, 4)
+
+        // reject verses that are changing the song too much
+        if (
+          verse.length === 4 &&
+          verse.every((line) => line.length > 15) &&
+          verse.every(
+            (line) => !line.toLowerCase().includes(instruction.toLowerCase())
+          )
+        ) {
+          return verse
+            .join('\n')
+            .replaceAll('<br>', '')
+            .replaceAll('<br/>', '')
+            .replace(/^#+/g, '')
+        }
+      }
+    } catch (err) {
+      // console.error(err)
+    }
+
+    // try again in a bit
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.min(10000, 100 + Math.pow(2, i)))
+    )
+  }
+
+  throw new Error('Failed to generate verse')
+}
--- song/index.ts
+++ song/index.ts
@@ -1,49 +1,8 @@
-import { Configuration, OpenAIApi } from 'openai'
 import * as original from './verses'
 import { numberToWords } from '../lib/numbers'
+import { editVerse } from './edit'
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-const openai = new OpenAIApi(configuration)
-
-async function editVerse(
-  input: string,
-  instruction: string,
-  temperature: number
-) {
-  let i = 0
-  while (i < 20) {
-    try {
-      const response = await openai.createEdit({
-        model: 'text-davinci-edit-001',
-        input,
-        instruction,
-        temperature,
-        top_p: 1,
-      })
-      if (response.status === 200 && response.data.choices.length > 0) {
-        const verse = response.data.choices[0].text.split('\n').slice(0, 4)
-
-        // reject verses that are changing the song too much
-        if (verse.length === 4 && verse.every((line) => line.length > 15)) {
-          return verse.join('\n')
-        }
-      }
-    } catch (err) {
-      // console.error(err)
-    }
-
-    // try again in a bit
-    await new Promise((resolve) =>
-      setTimeout(resolve, Math.min(10000, 100 + Math.pow(2, i)))
-    )
-  }
-
-  throw new Error('Failed to generate verse')
-}
-
-function pickVariation(input: string, iteration: bigint) {
+async function pickVariation(input: string, iteration: bigint) {
   return editVerse(
     input,
     'Input is a verse in a never ending song. Write the next verse for the song and make it rhyme.',
