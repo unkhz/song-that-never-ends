aef680cd12a8e57ead8a0def76a81d2d013c3d0b
Juhani Pelli <juhani.pelli@gmail.com>
2023-06-28 20:30:55 +0300

    Avoid spamming processes

--- lib/apple.ts
+++ lib/apple.ts
@@ -2,5 +2,6 @@ import { wait } from './tools'
 
 export async function say(line: string, rate: number, voice: string) {
   await wait(10)
-  Bun.spawn(['say', `-v${voice}`, `-r${rate}`, `"\n${line}"`])
+  const proc = Bun.spawn(['say', `-v${voice}`, `-r${rate}`, `"\n${line}"`])
+  return proc.exited
 }
