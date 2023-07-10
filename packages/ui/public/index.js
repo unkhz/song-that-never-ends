1f710ea572104e4fa55bd350c7c9273823c5c78a
Juhani Pelli <juhani.pelli@gmail.com>
2023-07-10 22:40:04 +0300

    Fix crossfade

--- packages/ui/public/index.js
+++ packages/ui/public/index.js
@@ -6,62 +6,100 @@ function createAudioElement(file, volume) {
   return audioEl
 }
 
-function createTrack(audioContext, audioEl, gainValue) {
-  const track = audioContext.createMediaElementSource(audioEl)
-
+function createGain(audioContext, gainValue) {
   const gain = audioContext.createGain()
   gain.gain.value = gainValue
+  return { in: gain, gain, out: gain }
+}
 
-  track.connect(gain)
-
-  return { in: track, gain, out: gain }
+function createTrack(audioContext, audioEl, gainValue) {
+  const track = audioContext.createMediaElementSource(audioEl)
+  const gain = createGain(audioContext, gainValue)
+  track.connect(gain.in)
+  return { in: track, gain: gain.in, out: gain.out }
 }
 
 function createDelay(audioContext, delayValue, gainValue) {
   // Now, create a gain node (to control the volume of the echo)
-  const gain = audioContext.createGain()
+  const gain = createGain(audioContext, gainValue)
 
   // And a delay node
   const delay = audioContext.createDelay(60)
-
-  // The amount of time in seconds for the initial echo delay
   delay.delayTime.value = delayValue
-
-  // The amount of volume reduction for each echo. This is in the range [0.0, 1.0]
-  gain.gain.value = gainValue
-
-  delay.connect(gain)
+  delay.connect(gain.in)
 
   // Connect the gain back to the delay to create a feedback loop
-  gain.connect(delay)
+  gain.out.connect(delay)
 
-  return { in: delay, gain, out: gain }
+  return { in: delay, gain: gain.in, out: gain.out }
 }
 
 function createCompressor(audioContext) {
   // Add compressor to keep constant volume
   const compressor = audioContext.createDynamicsCompressor()
   compressor.threshold.value = -50
-  compressor.knee.value = 20
-  compressor.ratio.value = 15
-  compressor.attack.value = 0
-  compressor.release.value = 1
+  compressor.knee.value = 10
+  compressor.ratio.value = 18
+  compressor.attack.value = 0.1
+  compressor.release.value = 0.2
 
   return { in: compressor, out: compressor }
 }
 
-function crossfade(audioContext, oldTrack, newTrack, durationSeconds) {
+function startCrossfade(
+  audioContext,
+  oldTrack,
+  newTrack,
+  durationSeconds,
+  targetGainValue
+) {
   newTrack.gain.gain.setValueAtTime(0, audioContext.currentTime)
 
-  // When the crossfade starts, reduce the volume of source 1 to 0 and increase the volume of source 2 to 1
+  // When the startCrossfade starts, reduce the volume of source 1 to 0 and increase the volume of source 2 to 1
   oldTrack.gain.gain.linearRampToValueAtTime(
     0,
     audioContext.currentTime + durationSeconds
   )
   newTrack.gain.gain.linearRampToValueAtTime(
-    1,
+    targetGainValue,
     audioContext.currentTime + durationSeconds
   )
+}
+
+function playVoice(audioContext, audioSrc) {
+  const audioEl = createAudioElement(audioSrc, 1)
+  const track = createTrack(audioContext, audioEl, 1)
+  const delay = createDelay(audioContext, 0.5, 0.22)
+  track.out.connect(delay.in)
+  delay.out.connect(audioContext.destination)
+
+  audioEl.addEventListener('ended', () => {
+    audioEl.remove()
+  })
+  audioEl.play()
+}
+
+let oldMusicTrack
+function playMusic(audioContext, audioSrc) {
+  const audioEl = createAudioElement(audioSrc, 1)
+  const track = createTrack(audioContext, audioEl, 1)
+
+  if (oldMusicTrack) {
+    startCrossfade(audioContext, oldMusicTrack, track, 3, 1)
+  }
+
+  // const delay = createDelay(audioContext, 1, 0.33)
+  const compressor = createCompressor(audioContext)
+  const masterGain = createGain(audioContext, 0.15)
+  track.out.connect(compressor.in)
+  compressor.out.connect(masterGain.in)
+  masterGain.out.connect(audioContext.destination)
+
+  audioEl.addEventListener('ended', () => {
+    audioEl.remove()
+  })
+  audioEl.play()
+  oldMusicTrack = track
 }
 
 let el
@@ -105,35 +143,15 @@ async function read() {
       const chars = new TextDecoder().decode(chunk).slice(0, -1)
       for (const char of chars.split('\n')) {
         hist = `${hist}${char}`.slice(-8)
-        let oldTrack
         if (char.startsWith('play:')) {
           if (!isSnapshot) {
             const audioSrc = char.slice(5)
             const isVoice = audioSrc.startsWith('audio/voice')
-            const audioEl = createAudioElement(audioSrc, isVoice ? 1 : 0.38)
-            const track = createTrack(audioContext, audioEl, 1)
-
-            if (oldTrack) {
-              crossfade(audioContext, oldTrack, track, 2)
-            }
-
-            if (!isVoice) {
-              const delay = createDelay(audioContext, 1, 0.33)
-              const compressor = createCompressor(audioContext)
-              track.out.connect(compressor.in)
-              compressor.out.connect(delay.in)
-              delay.out.connect(audioContext.destination)
+            if (isVoice) {
+              playVoice(audioContext, audioSrc)
             } else {
-              const delay = createDelay(audioContext, 0.5, 0.22)
-              track.out.connect(delay.in)
-              delay.out.connect(audioContext.destination)
+              playMusic(audioContext, audioSrc)
             }
-
-            audioEl.addEventListener('ended', () => {
-              audioEl.remove()
-            })
-            audioEl.play()
-            oldTrack = track
           }
         } else {
           if (hist.endsWith('<br><br>')) {
